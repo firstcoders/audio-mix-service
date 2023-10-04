084176ee94be7a1fbe3f82b4365bd033565699d0
sound-ws <mark@firstcoders.co.uk>
2023-10-04 06:31:10 +0000

    Update from https://github.com/firstcoders/monorepo/commit/e5ddf162e852b10c00985a9b1dffd1b8e52ca699

---
+++ build/3rd-party-licenses.mjs
@@ -0,0 +1,55 @@
+import * as esbuild from 'esbuild';
+import esbuildPluginLicense from 'esbuild-plugin-license';
+import { copyFileSync, rmSync } from 'fs';
+
+await esbuild.build({
+  entryPoints: ['src/index.js'],
+  outdir: 'licenses',
+  target: 'es2020',
+  format: 'esm',
+  external: ['aws-sdk'],
+  plugins: [
+    esbuildPluginLicense({
+      banner: `/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.license %> */`,
+      thirdParty: {
+        includePrivate: false,
+        output: {
+          file: 'THIRD-PARTY-LICENSES.txt',
+          // Template function that can be defined to customize report output
+          template(dependencies) {
+            return dependencies
+              .map(
+                (dependency) =>
+                  `Package: ${dependency.packageJson.name}\nVersion: ${dependency.packageJson.version}\nLicense: ${dependency.packageJson.license}\n\n${dependency.licenseText}`,
+              )
+              .join('\n');
+          },
+        },
+      },
+    }),
+  ],
+  bundle: true,
+  platform: 'node',
+});
+
+copyFileSync(
+  'licenses/THIRD-PARTY-LICENSES.txt',
+  './.aws-sam/build/AuthSignedUrlFunction/THIRD-PARTY-LICENSES.txt',
+);
+
+copyFileSync(
+  './licenses/THIRD-PARTY-LICENSES.txt',
+  './.aws-sam/build/CreateMixFunction/THIRD-PARTY-LICENSES.txt',
+);
+
+copyFileSync(
+  './licenses/THIRD-PARTY-LICENSES.txt',
+  './.aws-sam/build/GetStatusFunction/THIRD-PARTY-LICENSES.txt',
+);
+
+copyFileSync(
+  './licenses/THIRD-PARTY-LICENSES.txt',
+  './.aws-sam/build/MixAudioFunction/THIRD-PARTY-LICENSES.txt',
+);
+
+rmSync('./licenses', { recursive: true });
